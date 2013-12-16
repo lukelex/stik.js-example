bd28ff9472e63fdbb6f336a832104370c039dba2
Lukas Alexandre <lukas@codelogic.me>
2013-12-16 18:01:21 -0500

    Todo Almost done

--- js/todo.js
+++ js/todo.js
@@ -4,22 +4,25 @@ window.todoApp || (window.todoApp = {});
   var template = '<li data-controller="TodosCtrl" ' +
                      'data-action="Show">' +
     '<div class="view">' +
-      '<input class="edit" type="checkbox" />' +
+      '<input class="toggle" type="checkbox" />' +
       '<label data-bind="task"></label>' +
       '<button class="destroy"></button>' +
     '</div>' +
-    '<input class="edit" value="{{}}" />' +
+    '<input class="edit" value="" />' +
   '</li>';
 
   function Todo(args){
+    this.id = '#' + Math.floor(
+      Math.random()*16777215
+    ).toString(16);
+
     this.task = args.task;
+    this.completed = false;
   }
 
   Todo.prototype.view = function(){
-    return new DOMParser().parseFromString(
-      template.replace(/\{\{\}\}/gm, this.task),
-      "text/xml"
-    ).firstChild;
+    return new DOMParser().
+      parseFromString(template, "text/html").body.firstChild;
   }
 
   todoApp.Todo = Todo;
--- js/todos_ctrl.js
+++ js/todos_ctrl.js
@@ -1,48 +1,120 @@
 stik.register("TodosCtrl", "List", function($template, $courier, $viewBag){
-  var tpl, todosElms, todos, footer;
+  var self, tpl, todosElms, footer;
 
-  tpl       = $($template);
-  todosElms = tpl.getElementById('todo-list');
-  footer    = tpl.getElement('footer');
+  self = this;
+  self.todos = [];
 
-  function updateList(todo){
-    $(todo.view()).inject(todosElms);
+  tpl             = $($template);
+  listElm         = tpl.getElementById("todo-list");
+  footer          = tpl.getElement("footer");
+  completedMarker = tpl.getElementById("clear-completed");
+
+  function addTodo(todo){
+    self.todos.push(todo);
+    $(todo.view()).inject(listElm);
 
     stik.bindLazy();
     $courier.$send("bind-todo", todo);
 
-    todosElms.setStyle("display", "block");
+    listElm.setStyle("display", "block");
 
-    toggleFooter();
+    updateFooter();
   }
 
-  function toggleFooter(){
-    var todos = todosElms.getElements('li');
+  function removeTodo(todo){
+    self.todos = self.todos.filter(function(td){
+      return td.id !== todo.id;
+    });
 
-    if (todos.length > 0) {
+    updateFooter();
+  }
+
+  function updateFooter(){
+    if (self.todos.length > 0) {
       footer.setStyle("display", "block");
     } else {
       footer.setStyle("display", "none");
     }
 
     $viewBag.$render({
-      todosSize: todos.length
+      todosSize: self.todos.length
+    });
+
+    updateCompletedCount();
+  }
+
+  function updateListUI(todo){
+    var index = self.todos.indexOf(todo);
+    self.todos[index] = todo;
+
+    updateCompletedCount();
+  }
+
+  function updateCompletedCount(){
+    var completed = self.todos.filter(function(todo){
+      return todo.completed;
+    });
+
+    if (completed.length > 0) {
+      completedMarker.setStyle("display", "block");
+    } else {
+      completedMarker.setStyle("display", "none");
+    }
+
+    $viewBag.$render({
+      completed: completed.length
     });
   }
 
-  $courier.$receive("todo-created", updateList);
+  $courier.$receive("todo-created", addTodo);
+  $courier.$receive("todo-removed", removeTodo);
+  $courier.$receive("todo-updated", updateListUI);
 });
 
 stik.register("TodosCtrl", "Show", function($template, $viewBag, $courier){
-  var tpl = $($template);
-  var label = tpl.getElement("label");
+  var self      = this;
+  var tpl       = $($template);
+  var toggle    = tpl.getElement(".toggle");
+  var label     = tpl.getElement("label");
+  var editInput = tpl.getElement(".edit");
+  var remove    = tpl.getElement(".destroy");
 
-  $courier.$receive("bind-todo", function(todo){
-    $viewBag.$render(todo);
+  function updateView(todoData){
+    $viewBag.$render(todoData);
+  }
+
+  toggle.addEvent("change", function(event){
+    self.todo.completed = !self.todo.completed;
+    tpl.toggleClass("completed", self.todo.completed);
+    $courier.$send("todo-updated", self.todo);
   });
 
   label.addEvent("dblclick", function(event){
-    event.target.getParent("li").addClass("editing");
+    tpl.addClass("editing");
+    editInput.focus();
+  });
+
+  editInput.addEvent("keyup", function(event){
+    if (event.key === "enter") {
+      self.todo.task = event.target.value;
+
+      updateView(self.todo);
+
+      tpl.removeClass("editing");
+    }
+  }).addEvent("blur", function(event){
+    tpl.removeClass("editing");
+  });
+
+  remove.addEvent("click", function(){
+    $courier.$send("todo-removed", self.todo);
+    tpl.destroy();
+  });
+
+  var unsubscribe = $courier.$receive("bind-todo", function(todoData){
+    self.todo = todoData;
+    updateView(self.todo);
+    unsubscribe();
   });
 });
 
@@ -50,7 +122,7 @@ stik.register("TodosCtrl", "New", function($template, $courier){
   var tpl = $($template);
   var input = tpl.getElement("input");
 
-  input.addEvent("keydown", function(event){
+  input.addEvent("keyup", function(event){
     if (event.key === "enter") {
       var newTodo = new todoApp.Todo({task: event.target.value});
 
